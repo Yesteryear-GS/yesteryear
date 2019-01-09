5c2e148d9f154c7ced642f2e8ad55be48fa265a7
dayoung31668@gmail.com <dayounglee@Dayoungs-MacBook-Pro.local>
2019-01-09 12:00:55 -0500

    Set up outline for userhome/home, working on products

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,16 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import Button from '@material-ui/core/Button'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
   const {email} = props
-
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <div id="home">
+        <section id="carousel">
+          <img src="./images/temp_img.jpg" />
+        </section>
+        <div id="container">
+          <div id="menu">
+            <Button className="buttons">Show All</Button>
+            <Button className="buttons">Snacks</Button>
+            <Button className="buttons">Technology</Button>
+            <Button className="buttons">Pop Culture</Button>
+            <Button className="buttons">Toys/Games</Button>
+          </div>
+          <div id="products">.</div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/routes.js
+++ client/routes.js
@@ -22,6 +22,7 @@ class Routes extends Component {
           {/* Routes placed here are available to all visitors */}
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
+          <Route exact path="/" component={UserHome} />
           {isLoggedIn && (
             <Switch>
               {/* Routes placed here are only available after logging in */}
