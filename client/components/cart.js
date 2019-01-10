88c45775881fd6e8a3461aedde7eadac623da227
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-10 16:17:10 -0500

    beginning redux cart integration

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import store from '../store/index'
+import axios from 'axios'
 
-const Cart = props => {
-  return (
-    <>
-      <h2>Cart</h2>
-    </>
-  )
+const Cart = async () => {
+  try {
+    const userId = store.getState().user.id
+    const {data} = await axios.get('api/users/' + userId + '/orders')
+    return (
+      <>
+        <h2>Cart</h2>
+      </>
+    )
+  } catch (err) {
+    console.error(err)
+  }
 }
 
 export default Cart
