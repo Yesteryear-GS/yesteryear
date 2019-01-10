eca986c24e694983a6371f009591df08988e0492
Gerson Espinal <gerson.espinal@gmail.com>
2019-01-10 12:47:50 -0500

    working on adding cart

---
+++ client/components/cart.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Cart = props => {
+  return (
+    <div className="product">
+      <p>Testing</p>
+      <p>Testing</p>
+      <p>Testing</p>
+    </div>
+  )
+}
+
+export default Cart
--- client/routes.js
+++ client/routes.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
 import PropTypes from 'prop-types'
-import {Login, Signup, UserHome} from './components'
+import {Login, Signup, UserHome, Cart} from './components'
 import {me} from './store'
 
 /**
@@ -23,6 +23,7 @@ class Routes extends Component {
           <Route exact path="/login" component={Login} />
           <Route exact path="/signup" component={Signup} />
           <Route exact path="/" component={UserHome} />
+          <Route exact path="/cart" component={Cart} />
           {isLoggedIn && (
             <Switch>
               {/* Routes placed here are only available after logging in */}
