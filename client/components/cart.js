41cb2ef5a4f45f0fa8c76e35b1b4dafe728f8b05
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-10 16:37:23 -0500

    made cart component stateful

--- client/components/cart.js
+++ client/components/cart.js
@@ -1,17 +1,29 @@
 import React, {Component} from 'react'
 import store from '../store/index'
 import axios from 'axios'
+import Thumbnail from './thumbnail'
 
 class Cart extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      cart: []
+    }
+  }
+
   async componentDidMount() {
     const userId = store.getState().user.id
     const {data} = await axios.get('api/users/' + userId + '/orders')
+    this.setState({cart: data})
   }
 
   render() {
     return (
       <>
         <h2>Cart</h2>
+        {this.state.cart.map(item => {
+          // for each item in the cart, render an item view
+        })}
       </>
     )
   }
