a456d6efbead9fd0784601106adce2f8cf78b5aa
Gerson Espinal <gerson.espinal@gmail.com>
2019-01-11 16:59:35 -0500

    removed console.log

--- server/auth/google.js
+++ server/auth/google.js
@@ -21,7 +21,6 @@ module.exports = router
 if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
   console.log('Google client ID / secret not found. Skipping Google OAuth.')
 } else {
-  console.log('>>>>>>>>>>>>>> ', process.env.GOOGLE_CLIENT_ID)
   const googleConfig = {
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
