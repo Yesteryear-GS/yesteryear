4ed9653c0c89c61dd36254ad4b0a13291b7249f1
Joseph Lionarons <lionsamurai@gmail.com>
2019-01-13 10:38:51 -0500

    working on redux checkout

--- client/components/checkout.js
+++ client/components/checkout.js
@@ -1,8 +1,13 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import axios from 'axios'
+import {getCart} from '../store/cart'
 
 class Checkout extends Component {
+  componentDidMount() {
+    this.props.getCurrentCart()
+  }
+
   render() {
     console.log(this.props.currentCart)
     return (
@@ -19,4 +24,12 @@ const mapStateToProps = state => {
   }
 }
 
-export default connect(mapStateToProps)(Checkout)
+const mapDispatchToProps = dispatch => {
+  return {
+    getCurrentCart: () => {
+      dispatch(getCart())
+    }
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Checkout)
