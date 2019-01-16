0a4db130344405c38893f5b7cb2fb5212fe3f552
dayoung31668@gmail.com <dayounglee@Dayoungs-MBP.fios-router.home>
2019-01-16 00:06:38 -0500

    broken atm

--- client/components/cartTable.js
+++ client/components/cartTable.js
@@ -1,59 +1,136 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Button from '@material-ui/core/Button'
+import {deleteItem} from '../store/cart'
+import {connect} from 'react-redux'
 
 function formatPrice(priceInt) {
   return '$' + (priceInt / 100).toFixed(2)
 }
 
-const CartTable = props => {
-  const {cart, products} = props
+// const CartTable = props => {
+//   console.log('PROPS IN TABLE', props, 'cartcontent', props.cart.content)
+//   const {cart, products} = props
 
-  return (
-    <table>
-      <thead>
-        <tr>
-          <td className="underlined">Item</td>
-          <td className="underlined">Image</td>
-          <td className="underlined">Quantity</td>
-          <td className="underlined">Price (ea.)</td>
-          <td className="underlined">Price</td>
-          <td className="underlined">Put back</td>
-        </tr>
-      </thead>
-      <tbody>
-        {cart &&
-          cart.content &&
-          cart.content.map((item, idx) => {
-            const cartItem = products[item.id - 1]
+//   return (
+//     <table>
+//       <thead>
+//         <tr>
+//           <td className="underlined">Item</td>
+//           <td className="underlined">Image</td>
+//           <td className="underlined">Quantity</td>
+//           <td className="underlined">Price (ea.)</td>
+//           <td className="underlined">Price</td>
+//           <td className="underlined">Put back</td>
+//         </tr>
+//       </thead>
+//       <tbody>
+//         {cart &&
+//           cart.content &&
+//           cart.content.map((item, idx) => {
+//             const cartItem = products[item.id - 1]
+//             const id = cartItem.id
+//             const name = cartItem.name
+//             const imgUrl = cartItem.imageUrl
+//             const qty = cart.content[idx].itemQuantity
+//             const price = cart.content[idx].price
+//             const adjPrice = price * qty
 
-            const name = cartItem.name
-            const imgUrl = cartItem.imageUrl
-            const qty = cart.content[idx].itemQuantity
-            const price = cart.content[idx].price
-            const adjPrice = price * qty
+//             return (
+//               <tr key={'cart-item-' + item.id}>
+//                 <td>{name}</td>
+//                 <td>
+//                   <img className="cart-thumbnail" src={imgUrl} />
+//                 </td>
 
-            return (
-              <tr key={'cart-item-' + item.id}>
-                <td>{name}</td>
-                <td>
-                  <img className="cart-thumbnail" src={imgUrl} />
-                </td>
+//                 <td>{qty}</td>
 
-                <td>{qty}</td>
+//                 <td>{formatPrice(price)}</td>
+//                 <td>{formatPrice(adjPrice)}</td>
+//                 <td>
+//                   <Button
+//                     onClick={() => {
+//                       props.deleteThisItem(id)
+//                       console.log('deleted')
+//                     }}
+//                   >
+//                     <i className="material-icons">remove_shopping_cart</i>
+//                   </Button>
+//                 </td>
+//               </tr>
+//             )
+//           })}
+//       </tbody>
+//     </table>
+//   )
+// }
 
-                <td>{formatPrice(price)}</td>
-                <td>{formatPrice(adjPrice)}</td>
-                <td>
-                  <Button>
-                    <i className="material-icons">remove_shopping_cart</i>
-                  </Button>
-                </td>
-              </tr>
-            )
-          })}
-      </tbody>
-    </table>
-  )
+class CartTable extends Component {
+  constructor(props) {
+    super()
+    console.log('PPPPROPS', props)
+  }
+
+  render() {
+    let cart = this.props.cart.content
+    console.log('cart in TABLE', cart)
+    return (
+      <table>
+        <thead>
+          <tr>
+            <td className="underlined">Item</td>
+            <td className="underlined">Image</td>
+            <td className="underlined">Quantity</td>
+            <td className="underlined">Price (ea.)</td>
+            <td className="underlined">Price</td>
+            <td className="underlined">Put back</td>
+          </tr>
+        </thead>
+        <tbody>
+          {cart &&
+            cart.map((item, idx) => {
+              const cartItem = this.props.products[item.id - 1]
+              const id = cartItem.id
+              const name = cartItem.name
+              const imgUrl = cartItem.imageUrl
+              const qty = this.props.cart.content[idx].itemQuantity
+              const price = this.props.cart.content[idx].price
+              const adjPrice = price * qty
+
+              return (
+                <tr key={'cart-item-' + item.id}>
+                  <td>{name}</td>
+                  <td>
+                    <img className="cart-thumbnail" src={imgUrl} />
+                  </td>
+
+                  <td>{qty}</td>
+
+                  <td>{formatPrice(price)}</td>
+                  <td>{formatPrice(adjPrice)}</td>
+                  <td>
+                    <Button
+                      onClick={() => {
+                        this.props.deleteThisItem(cartItem)
+                      }}
+                    >
+                      <i className="material-icons">remove_shopping_cart</i>
+                    </Button>
+                  </td>
+                </tr>
+              )
+            })}
+        </tbody>
+      </table>
+    )
+  }
 }
 
-export default CartTable
+const mapDispatchToProps = dispatch => {
+  return {
+    deleteThisItem: item => {
+      dispatch(deleteItem(item))
+    }
+  }
+}
+
+export default connect(null, mapDispatchToProps)(CartTable)
--- client/store/cart.js
+++ client/store/cart.js
@@ -7,6 +7,7 @@ const initialState = {
 
 const GOT_CART = 'GOT_CART'
 const REMOVE_CART = 'REMOVE_CART'
+const DELETE_CART = 'DELETE_CART'
 
 const gotCart = cart => {
   return {
@@ -19,6 +20,13 @@ export const removeCart = () => {
   return {
     type: REMOVE_CART,
     content: []
+  }
+}
+
+export const putBack = item => {
+  return {
+    type: DELETE_CART,
+    item
   }
 }
 
@@ -39,12 +47,30 @@ export const getCart = () => {
   }
 }
 
+export const deleteItem = item => {
+  return dispatch => {
+    try {
+      dispatch(putBack(item))
+    } catch (error) {
+      console.error(error)
+    }
+  }
+}
+
 const cartReducer = (state = initialState, action) => {
+  let newCart
+  let cart
   switch (action.type) {
     case GOT_CART:
       return {...state, content: action.cart}
     case REMOVE_CART:
       return {...state, content: action.cart}
+    case DELETE_CART:
+      cart = state.content
+      console.log('CART IN STORE', cart)
+      newCart = cart.filter(item => item.id !== action.item.id)
+      console.log('newbebwbw', newCart, 'action', action.item)
+      return {...state, content: newCart}
     default:
       return state
   }
